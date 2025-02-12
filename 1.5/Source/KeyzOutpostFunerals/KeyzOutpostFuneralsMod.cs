c95cdd846e413814f1d5ca85dc01069cd41ce4db
Kieran David Evans <keyz182@gmail.com>
2025-02-12 08:20:00 +0000

    Cookie cutter initial commit

---
+++ 1.5/Source/KeyzOutpostFunerals/KeyzOutpostFuneralsMod.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace KeyzOutpostFunerals;
+
+public class KeyzOutpostFuneralsMod : Mod
+{
+
+    public KeyzOutpostFuneralsMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from KeyzOutpostFunerals");
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.KeyzOutpostFunerals.main");	
+        harmony.PatchAll();
+    }
+}
