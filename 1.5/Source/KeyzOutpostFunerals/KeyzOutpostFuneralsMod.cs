051afafbc1acd717d43c8f1f700d81212a8575cb
Kieran David Evans <keyz182@gmail.com>
2025-02-12 08:37:07 +0000

    Add functionality to handle outpost funerals for dead pawns
    Implemented handling for transferring corpses from outposts to player colonies for funeral arrangements. Updated dependencies, added mod configuration, localization files, and integrated the functionality into project structure with appropriate Harmony patches.

--- 1.5/Source/KeyzOutpostFunerals/KeyzOutpostFuneralsMod.cs
+++ 1.5/Source/KeyzOutpostFunerals/KeyzOutpostFuneralsMod.cs
@@ -9,11 +9,10 @@ public class KeyzOutpostFuneralsMod : Mod
 
     public KeyzOutpostFuneralsMod(ModContentPack content) : base(content)
     {
-        Log.Message("Hello world from KeyzOutpostFunerals");
 #if DEBUG
         Harmony.DEBUG = true;
 #endif
-        Harmony harmony = new Harmony("keyz182.rimworld.KeyzOutpostFunerals.main");	
+        Harmony harmony = new Harmony("keyz182.rimworld.KeyzOutpostFunerals.main");
         harmony.PatchAll();
     }
 }
